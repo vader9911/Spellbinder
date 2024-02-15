6d1286765c5869d53d035faa6b521f23c3859902
unknown <gabebarraza33@gmail.com>
2024-02-14 19:38:40 -0600

    More code to collection model

--- models/Collection.js
+++ models/Collection.js
@@ -4,5 +4,20 @@ const sequelize = require('../config/connection');
 class collection extends Model{}
 
 collection.init(
-    
-)
+   // TODO : define fields and columns for the cards 
+    {
+        cards: {
+        type: DataTypes.string 
+        }
+    },
+    {
+        // TODO : Link to database connection
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'card'
+      }
+
+);
+
+module.exports = Card;
