ecd81051ffdf7280d4dcc0a70b05773ce983f7e0
unknown <gabebarraza33@gmail.com>
2024-02-15 19:49:47 -0600

    adding files to models

--- models/Collection.js
+++ models/Collection.js
@@ -13,10 +13,6 @@ Collection.init(
             primaryKey: true,
         },
 
-        cards: {
-            type: DataTypes.STRING,
-        },
-
         user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
