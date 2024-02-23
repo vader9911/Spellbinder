8e6abf9172cceebdec93e416d1629d4fd96796f8
kev-rod43 <148256276+kev-rod43@users.noreply.github.com>
2024-02-22 22:48:24 -0500

    Update cardRoutes.js

--- controllers/api/cardRoutes.js
+++ controllers/api/cardRoutes.js
@@ -3,37 +3,37 @@ const router = express.Router();
 const { Card } = require('../../models');
 const { CollectionCard } = require('../../models');
 //Query database for all cards
-router.get('/', async (req, res) => {
-    try {
-        const cardData = await Card.findAll();
-        res.status(200).json(cardData);
-    } catch (err) {
-        res.status(2004).end()
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const cardData = await Card.findAll();
+//         res.status(200).json(cardData);
+//     } catch (err) {
+//         res.status(2004).end()
+//     }
+// });
 
-router.post('/findorcreate', async (req, res) => {
-    try {
+// router.post('/findorcreate', async (req, res) => {
+//     try {
 //When the user clicks on a card, check to see if card exists, if not, create it.
     
-        const card = await Card.findOrCreate({
-            where: {
-                scryfall_id: req.body.uuid
-            },
-            defaults: {
-                oracle_text: req.body.oracle_text,
-                rarity: req.body.rarity,
-                card_name: req.body.card_name,
-                img_uri: req.body.img_uri,
-                card_price: req.body.card_price
-            }
-        });
-        //not seeing card returned
-        res.status(200).json(card);
-    } catch (err) {
-        res.status(2004).end()
-    }
-})
+//         const card = await Card.findOrCreate({
+//             where: {
+//                 scryfall_id: req.body.uuid
+//             },
+//             defaults: {
+//                 oracle_text: req.body.oracle_text,
+//                 rarity: req.body.rarity,
+//                 card_name: req.body.card_name,
+//                 img_uri: req.body.img_uri,
+//                 card_price: req.body.card_price
+//             }
+//         });
+//         //not seeing card returned
+//         res.status(204).end());
+//     } catch (err) {
+//         res.status(2004).end()
+//     }
+// })
 //Route for adding to a collection
 /*
 User views cards
@@ -56,16 +56,21 @@ router.post('/addtocollection', async (req, res) => {
 
             }
         });
+        const userCollection = await Collection.findOne({
+            where: {
+                user_id: req.session.user_id,
+            }
+        })
 //make a pairing between a card and a collection; and query the database
 //find the user thats logged in and their collection
         const collectedCard = await CollectionCard.create({
-            condition: req.body.condition,
-            collection_id: req.body.collection_id,
-            card_id: req.body.card_id,
+            //condition: req.body.condition,
+            collection_id: userCollection.id,
+            card_id: card.id,
         })
-        res.status(2004).end()
+        res.status(204).end()
     } catch (err) {
-        res.status(200).json()
+        res.status(500).json("Internal server error")
     }
 })
 
