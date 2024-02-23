05b7829e6d58350dc37c5c389410b6687744fa6e
unknown <gabebarraza33@gmail.com>
2024-02-22 20:11:18 -0600

    Adding the withall and changing the req parameter

--- controllers/api/collectionRoutes.js
+++ controllers/api/collectionRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const { Collection } = require('../../models');
 
 // Creats a collection if the user does not already have one. Also Looks for one.
-  router.get('/:userid', withAll, async (req, res) =>{
-    try{
-      const collectionData = await Collection.findOrCreate ({
-        where:{
-          user_id: req.session.userid 
-        }
-      });
-      res.status(200).json(collectionData);
-    } catch(err) {
-      res.status(500).json(err);
-    };
-  });
+router.get('/:userid', withAll, async (req, res) =>{
+  try{
+    const collectionData = await Collection.findOrCreate ({
+      where:{
+        user_id: req.session.userid 
+      }
+    });
+    res.status(200).json(collectionData);
+  } catch(err) {
+    res.status(500).json(err);
+  };
+});
 
 // // DELETES a collection
 // router.delete('/:id', async (req, res) => {
